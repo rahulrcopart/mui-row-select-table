80ec963445e086ae0f01d7c4c74095c9583b899e
Rahul Ravindran <rahul.ravindran@copart.com>
2018-07-04 12:58:02 -0300

    Fixed build

--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,7 @@ const { join, resolve } = path
 
 const root = resolve(__dirname)
 const src = join(root, 'src')
-const dest = join(root, 'dist')
+const dest = join(root, '.')
 
 module.exports = {
   devtool: 'source-map',
